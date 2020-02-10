bafbc982b9c193767e711b1ab240572a93052b1d
Jack-Meade <jack-meade@outlook.com>
2020-02-10 18:19:07 +0000

    Add JavaDoc to MyGit.java

--- MyGit.java
+++ MyGit.java
@@ -1,14 +1,26 @@
 import java.util.*;
 import java.io.*;
 
+/**
+* MyGit class, alternative git command interpreter
+* @author Ronan M
+* @version 1.0
+*/
 public class MyGit {
   ProcessBuilder builder = new ProcessBuilder();
   HashMap<String, Integer> files = new HashMap<String, Integer>();
 
+  /**
+  * Constructor for Builder class, initialised as empty string
+  */
   public MyGit() {
     this.files = files;
   }
 
+  /**
+  * Runs command(s) given
+  * @param command string which will interpreted
+  */
   public void runCommand(String command) throws IOException {
     Process pr = Runtime.getRuntime().exec(command);
 
@@ -21,20 +33,34 @@ public class MyGit {
     }
   }
 
+  /**
+  * Initialises git repository
+  */
   public void init() throws IOException {
     runCommand("git init");
   }
 
+  /**
+  * Adds file to index
+  * @param name filename of file to be added
+  */
   public void add(String name) throws IOException {
     String versionString = String.format("%s-Version%d", name, this.files.get(name));
     String addCommand = String.format("git add %s", versionString);
     runCommand(addCommand);
   }
 
+  /**
+  * Commits staged files to local repository
+  */
   public void commit() throws IOException {
     runCommand("git commit -m new_commit");
   }
 
+  /**
+  * Creates file with a version of 1
+  * @param name filename of file to be created
+  */
   public void create(String name) throws IOException{
     this.files.put(name, 1);
     String versionString = String.format("%s-Version%d", name, this.files.get(name));
@@ -42,6 +68,10 @@ public class MyGit {
     runCommand(createCommand);
   }
 
+  /**
+  * Update file and its version
+  * @param name filename of file to be updated 
+  */
   public void touch(String name) throws IOException{
     String oldVersionString = String.format("%s-Version%d", name, this.files.get(name));
     this.files.put(name, this.files.get(name) + 1);
